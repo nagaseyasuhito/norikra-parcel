ec3b69c29b5e69c4de71849dfbc0fcb3f2f6ccaf
nagaseyasuhito <nagase@nagaseyasuhito.net>
2014-07-28 18:28:16 +0900

    add alternatives for cli tools(jruby, norikra, norikra-client)

--- src/main/resources/meta/norikra_alternatives.sh
+++ src/main/resources/meta/norikra_alternatives.sh
@@ -4,10 +4,12 @@ FINAL_NAME="${project.build.finalName}"
 NORIKRA_DIRNAME=${PARCEL_DIRNAME:-$FINAL_NAME}
 
 if [[ "$1" == "activate" ]]; then
-  update-alternatives --install /usr/bin/norikra norikra $PARCELS_ROOT/$NORIKRA_DIRNAME/gem/bin/norikra $PRIORITY
-  update-alternatives --install /usr/bin/norikra-client norikra-client $PARCELS_ROOT/$NORIKRA_DIRNAME/gem/bin/norikra-client $PRIORITY
+  update-alternatives --install /usr/bin/jruby jruby $PARCELS_ROOT/$NORIKRA_DIRNAME/bin/jruby $PRIORITY
+  update-alternatives --install /usr/bin/norikra norikra $PARCELS_ROOT/$NORIKRA_DIRNAME/bin/norikra $PRIORITY
+  update-alternatives --install /usr/bin/norikra-client norikra-client $PARCELS_ROOT/$NORIKRA_DIRNAME/bin/norikra-client $PRIORITY
 
 elif [[ "$1" == "deactivate" ]]; then
+  update-alternatives --remove jruby $PARCELS_ROOT/$NORIKRA_DIRNAME/bin/jruby
   update-alternatives --remove norikra $PARCELS_ROOT/$NORIKRA_DIRNAME/bin/norikra
   update-alternatives --remove norikra-client $PARCELS_ROOT/$NORIKRA_DIRNAME/bin/norikra-client
 
